ac77bdbfb19f7f5da94d3385b873e8b368a85aad
Jaivardhan Singh Rathore <jaivardhan.rathore2005@gmail.com>
2026-09-09 01:32:10 +0530

    Time: 68 ms (76.74%), Space: 62.3 MB (76.05%) - LeetHub

---
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -0,0 +1,15 @@
+var checkIfInstanceOf = function(obj, classFunction) {
+    if (classFunction == null) {
+        return false;
+    }
+
+    while (obj != null) {
+        if (Object.getPrototypeOf(obj) === classFunction.prototype) {
+            return true;
+        }
+
+        obj = Object.getPrototypeOf(obj);
+    }
+
+    return false;
+};
